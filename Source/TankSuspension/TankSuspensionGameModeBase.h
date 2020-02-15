5cc55fef49e96f39cb05b62a9383494414bbb35c
Mohammed Abushawish <59448589+mkscit@users.noreply.github.com>
2020-02-15 21:44:00 +0200

    Initial commit

---
+++ Source/TankSuspension/TankSuspensionGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankSuspensionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSUSPENSION_API ATankSuspensionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
